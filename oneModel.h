3937a77ff8930dbef3fc86ae148de3c6f10a6ad3
jiangshilin <jsl328@yeah.net>
2016-01-14 16:36:50 +0800

    two commit

---
+++ oneModel.h
@@ -0,0 +1,13 @@
+//
+//  oneModel.h
+//  ViewLayout
+//
+//  Created by jiangshilin on 16/1/14.
+//  Copyright © 2016年 jiangshilin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface oneModel : NSObject
+
+@end
---
+++ oneModel.m
@@ -0,0 +1,13 @@
+//
+//  oneModel.m
+//  ViewLayout
+//
+//  Created by jiangshilin on 16/1/14.
+//  Copyright © 2016年 jiangshilin. All rights reserved.
+//
+
+#import "oneModel.h"
+
+@implementation oneModel
+
+@end
